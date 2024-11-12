3554f03e7dc4459e6c5ce32ca6b7c299dfcc1797
liangpengyv <me@laoliang.ink>
2024-11-12 19:14:12 +0800

    feat: 添加饼图

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,25 @@
         <GettingStarted />
       </div>
     </details>
+
     <details>
       <summary>基础折线图</summary>
       <div class="chart-wrapper">
         <BasicLineChart />
       </div>
     </details>
+
     <details>
       <summary>基础柱状图</summary>
       <div class="chart-wrapper">
         <BasicBar />
+      </div>
+    </details>
+
+    <details>
+      <summary>某站点用户Access From</summary>
+      <div class="chart-wrapper">
+        <RefererOfAWebsite />
       </div>
     </details>
   </div>
@@ -26,6 +35,7 @@
 import BasicBar from './components/bar/BasicBar.vue'
 import GettingStarted from './components/GettingStarted.vue'
 import BasicLineChart from './components/line/BasicLineChart.vue'
+import RefererOfAWebsite from './components/pie/RefererOfAWebsite.vue'
 </script>
 
 <style scoped>
